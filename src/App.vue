3bf22bafcd88ed42e761877ec348bd17fc1f6d32
ilya <murzyevilgiz@yandex.ru>
2021-03-20 19:07:21 +0300

    add navbar and carusel

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,52 @@
 <template>
   <v-app>
+    <MainMenu/>
 
     <v-main>
+      <v-carousel>
+        <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+
+        ></v-carousel-item>
+      </v-carousel>
     </v-main>
   </v-app>
 </template>
 
 <script>
 
+
+import MainMenu from "./components/MainMenu";
+
 export default {
   name: 'App',
 
-  components: {
-  },
+  components: {MainMenu},
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
 };
 </script>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-toolbar
+        dark
+        dense
+        prominent
+        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MainMenu'
+}
+</script>
